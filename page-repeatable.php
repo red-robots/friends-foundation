b86bc112653355c22dc0c35e6b6a1c388a930d1f
Lisa <lisaqdebona@gmail.com>
2024-06-07 04:37:59 -0400

    Styling testimonials section

--- page-repeatable.php
+++ page-repeatable.php
@@ -4,8 +4,10 @@
  */
 get_header(); ?>
 <div id="primary" class="content-area-full repeatable-layout ">
-	<main id="main" class="site-main" role="main" data-pagetitle="<?php echo get_the_title()?>">
-		<div class="wp-flexible-container"></div>
+	<main id="main" class="site-main" role="main" data-postid="<?php echo get_the_ID() ?>" data-pagetitle="<?php echo get_the_title()?>">
+		<?php if( have_rows('flexible_content') ) {  ?>
+      <?php get_template_part('parts/repeatable-blocks') ?>
+    <?php } ?>
 	</main><!-- #main -->
 </div><!-- #primary -->
 <?php
